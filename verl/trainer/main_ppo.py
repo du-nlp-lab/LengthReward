71e92b0eb8d04cfd625fab3ee407806990e57609
ruosenli <ruosenli@tencent.com>
2025-08-04 15:16:48 +0800

    update codes

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -163,6 +163,9 @@ class TaskRunner:
         elif reward_manager_name == 'dapo':
             from verl.workers.reward_manager import DAPORewardManager
             reward_manager_cls = DAPORewardManager
+        elif reward_manager_name == 'length_penalty':
+            from verl.workers.reward_manager import LPRewardManager
+            reward_manager_cls = LPRewardManager
         else:
 
             raise NotImplementedError
